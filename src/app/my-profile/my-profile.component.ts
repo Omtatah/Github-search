b5c447a5280add46a8870c363232675b3d7d6479
Omtatah <murtallahomtatah@gmail.com>
2019-06-08 16:20:16 +0300

    import the crucial components int the my-profile.components.ts

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import{User} from '../user';
+import { Repos } from '../repos';
+import { ProfileService } from '../services/profile.service';
+import{HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
