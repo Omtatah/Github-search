2224ffe60fb4bc652c2b78fc120f611ea4761556
Omtatah <murtallahomtatah@gmail.com>
2019-06-08 20:32:53 +0300

    define all functions necessary for the page to run on ngOnInit

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -10,10 +10,26 @@ import { Repos } from '../repos';
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
+  newSearch = new Search('','','',0);
+  public username ='';
+  repos:Repos;
+  user:User;
 
-  constructor() { }
+
+  submitUser(name){
+    this.profileService.getProfileInfo(name.value);
+    this.repositoryService.getProfileRepo(name.value);
+  }
+
+
+  constructor(private profileService:ProfileService,private repositoryService:ProfileService) { }
 
   ngOnInit() {
+    this.profileService.getProfileInfo(this.username);
+    this.user = this.profileService.user;
+
+    this.repositoryService.getProfileRepo(this.username);
+    this.repos = this.repositoryService.repos;
   }
 
 }
