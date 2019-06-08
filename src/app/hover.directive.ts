0bbb55221e1773fe51316894da651ad1b9b0adb8
Omtatah <murtallahomtatah@gmail.com>
2019-06-08 22:36:56 +0300

    generate the hover directive; generate the date-count pipe; code in the necessary instructions to run these two features and linked them to other pages via the export and import method

---
+++ src/app/hover.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appHover]'
+})
+export class HoverDirective {
+
+  constructor(private repoColor:ElementRef) { }
+  @HostListener('mouseenter') onMouseEnter(){
+    this.highlight('#E0FFFF');
+  }
+  @HostListener('mouseleave') onMouseLeave(){
+    this.highlight(null);
+}
+ private highlight(color:String){
+   this.repoColor.nativeElement.style.backgroundColor= color;
+
+ }
+}
