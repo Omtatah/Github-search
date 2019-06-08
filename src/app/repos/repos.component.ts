e660bf560f67437683a300de7cb4bcc0e7f8a1c4
Omtatah <murtallahomtatah@gmail.com>
2019-06-08 20:27:36 +0300

    properly indent the repos.component.ts file

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,28 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import{User} from '../user';
+import { User } from '../user';
 import { Repos } from '../repos';
 import { ProfileService } from '../services/profile.service';
-import{HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
-  styleUrls: ['./repos.component.css']
-  providers:[ProfileService]
+  styleUrls: ['./repos.component.css'],
+
+  providers: [ProfileService]
 })
 export class ReposComponent implements OnInit {
 
-  public username ='Omtatah';
-    repos:Repos;
-    user:User;
+  public username = 'Omtatah';
+  repos: Repos;
+  user: User;
 
-    constructor(private profileService:ProfileService,public repositoryService:ProfileService) { }
+  constructor(private profileService: ProfileService, public repositoryService: ProfileService) { }
 
-    ngOnInit() {
-      this.profileService.getProfileInfo(this.username);
-      this.user = this.profileService.user;
+  ngOnInit() {
+    this.profileService.getProfileInfo(this.username);
+    this.user = this.profileService.user;
 
-      this.repositoryService.getProfileRepo(this.username);
-      this.repos = this.repositoryService.repos;
-    }
-
+    this.repositoryService.getProfileRepo(this.username);
+    this.repos = this.repositoryService.repos;
   }
+
+}
