c8ef3c70f8a2db347d5b740c9ee51d4cc42c3c39
Omtatah <murtallahomtatah@gmail.com>
2019-06-08 17:03:05 +0300

    properly indent my code .

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -10,11 +10,11 @@ import { Repos } from '../repos';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  public username ='';
-  repos:Repos;
-  user:User;
+  public username = '';
+  repos: Repos;
+  user: User;
 
-  constructor(private profileService:ProfileService,public repositoryService:ProfileService) { }
+  constructor(private profileService: ProfileService, public repositoryService: ProfileService) { }
 
   ngOnInit() {
     this.profileService.getProfileInfo(this.username);
