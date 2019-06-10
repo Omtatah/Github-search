69d66242379a8a4b9c98d06196dce6a1fe8865e6
Omtatah <murtallahomtatah@gmail.com>
2019-06-10 09:48:18 +0300

    Auto-generated commit

--- src/app/repos.ts
+++
@@ -1,5 +0,0 @@
-export class Repos {
-  
-    constructor(public name:string,public description:string,public html_url:string,public created_at:Date,public updated_at:Date,public homepage:string) {}
-  }
-  
--- src/app/search.ts
+++
@@ -1,5 +0,0 @@
-export class Search {
-
-    constructor(public name: string, public followers: string, public following: string, public public_repos: number) { }
-
-}
